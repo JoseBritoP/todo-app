071a9d4b41e9978a5875104d877af25a48899ec4
JoseBritoP <josealbritose@gmail.com>
2023-08-15 16:20:35 -0300

    Todos

---
+++ src/components/Todos.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { type ListOfTodos } from '../types';
+
+interface Props {
+  todos: ListOfTodos;
+}
+
+const Todos: React.FC<Props> = ({ todos }) => {
+  return (
+    <ul>
+      {todos.map((todo) => (
+        <li key={todo.id}>
+          {todo.title}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default Todos;
